f2254671f5ca8f48935bbd906dc917586454f531
Alejandro Rojas <alejandrofabianrojas64@gmail.com>
2024-11-03 01:49:40 -0300

    the buttons were componentized

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/button.module.css'
+
+const button = () => {
+  return (
+    <button className={`${styles.button} rounded-pill px-4 py-2`}>Get Started</button>
+  )
+}
+
+export default button
