3f4c05129d2493d48e5e41ae51ae22a473c48939
Alejandro Rojas <alejandrofabianrojas64@gmail.com>
2024-11-04 06:06:48 -0300

    added testimonials component and initial styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import Info from "./components/Info"
 import Features from "./components/Features"
+import Testimonials from "./components/Testimonials"
 function App() {
 
   return (
@@ -8,6 +9,7 @@ function App() {
      <Header />
      <Info />
      <Features />
+     <Testimonials />
     </>
   )
 }
---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,57 @@
+import styles from '../styles/testimonials.module.css'
+
+type TestimonialsTypes = {
+    profile: string,
+    name: string,
+    testimony: string,
+    key: number
+}
+const testimonials: TestimonialsTypes[] = [
+    {
+        profile: '/assets/avatar-anisha.png',
+        name: 'Anisha Li',
+        testimony: '“Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”',
+        key: 1
+    },
+    {
+        profile: '/assets/avatar-ali.png',
+        name: 'Ali Bravo',
+        testimony: '“We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.”',
+        key: 2
+    },
+    {
+        profile: '/assets/avatar-richard.png',
+        name: 'Richard Watts',
+        testimony: '“Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!”',
+        key: 3
+    },
+    {
+        profile: '/assets/avatar-shanai.png',
+        name: 'Shanai Gough',
+        testimony: '“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”',
+        key: 4
+    }
+]
+
+const Testimonials = () => {
+    return (
+        <section className={`${styles.sliderContainer} mx-0 mt-5`}>
+            <div className={`${styles.slider} w-100 h-auto overflow-hidden`}>
+                <h2 className='text-center my-5'>What they’ve said</h2>
+                <div className={`${styles.slideTrack} d-flex`}>
+                    {testimonials.map(testimony => (
+                        <div key={testimony.key} className={styles.slide}>
+                            <img src={testimony.profile} alt={testimony.name} />
+                            <div className={styles.testimonyContainer}>
+                                <h4>{testimony.name}</h4>
+                                <p>{testimony.testimony}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Testimonials
