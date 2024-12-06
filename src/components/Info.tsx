9bbe5da012fd63cb174f77bf3d23f11bf9b8a982
Alejandro Rojas <alejandrofabianrojas64@gmail.com>
2024-12-06 01:26:58 -0300

    Responsive versions added for header and info sections

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -3,14 +3,14 @@ import Button from './Button'
 
 const Info = () => {
     return (
-        <section className='d-flex my-5 py-5'>
-            <div className={`my-5 w-50 ${styles.descriptionContainer}`}>
+        <section className='d-flex my-5'>
+            <div className={`${styles.descriptionContainer}`}>
                 <h1 className={`${styles.title}`}>Bring everyone together to build better products.</h1>
-                <p className={`${styles.descriptionInfo} w-75 mb-5`}>
+                <p className={`${styles.descriptionInfo}`}>
                     Manage makes it simple for software teams to plan day-to-day
                     tasks while keeping the larger team goals in view.
                 </p>
-                <Button otherStyles='' children='Get Started'/>
+                <Button otherStyles=''>Get Started</Button> 
             </div>
             <div className={styles.illustrationInfo}>
                 <img src="/assets/illustration-intro.svg" width={550} height={500} />
