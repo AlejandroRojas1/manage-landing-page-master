573e5b1f9a5ecb23b5da99311cfc3a7682be62e3
Alejandro Rojas <alejandrofabianrojas64@gmail.com>
2024-12-08 04:13:32 -0300

    finish the header menu for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/header.module.css'
 import Button from './Button'
 
 const Header = () => {
-    const [hamburguerMenu, setHamburguerMenu] = useState<boolean>(false)
+    const [displayedMenu, setDisplayedMenu] = useState<boolean>(false)
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     useEffect(() => {
@@ -18,10 +18,25 @@ const Header = () => {
         };
     }, []);
 
-    const navRef = useRef(null)
+    const navRef = useRef<HTMLDivElement>(null)
+
+    const handleDisplayedMenu = ()=> {
+        setDisplayedMenu(!displayedMenu)
+        if(navRef.current && windowWidth <= 875) {
+            !displayedMenu
+                ? (navRef.current.style.display = "flex")
+                : (navRef.current.style.display = "none")
+        }
+        
+    }
 
     return (
         <>
+            {
+               (displayedMenu && windowWidth <= 875) 
+                    && <div className={styles.overlay}></div>
+            }
+
             <nav className={`${styles.headerContainer} navbar pt-4`}>
                 <div className={`container-fluid ${styles.header} d-flex `}>
                     <a className="navbar-brand" href="#">
@@ -31,8 +46,12 @@ const Header = () => {
                     {
                         windowWidth <= 857 &&
                             <div className={styles.hamburguerMenu}>
-                                <button className={styles.hamburguerButton}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path fill="#000000" d="M3 18v-2h18v2zm0-5v-2h18v2zm0-5V6h18v2z"/></svg>
+                                <button onClick={handleDisplayedMenu} className={styles.hamburguerButton}>
+                                    {
+                                        displayedMenu
+                                            ? <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path fill="#1A2633" d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6z"/></svg>
+                                            : <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path fill="#1A2633" d="M3 18v-2h18v2zm0-5v-2h18v2zm0-5V6h18v2z"/></svg>
+                                    }
                                 </button>
                                 
                             </div>
