af869e63c603f63890ca8a2802bf36e44e8af864
Alejandro Rojas <alejandrofabianrojas64@gmail.com>
2024-12-08 12:13:47 -0300

    created context for provide screen size

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,40 @@ import Features from "./components/Features"
 import Testimonials from "./components/Testimonials"
 import GetProduct from "./components/GetProduct"
 import Footer from "./components/Footer"
+import { 
+  createContext, 
+  useState, 
+  useEffect 
+} from "react"
+
+export const windowSizeContext = createContext<number>(window.innerWidth);
 
 function App() {
+  
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+
+  useEffect(() => {
+      const handleResize = () => {
+          setWindowWidth(window.innerWidth);
+      };
+
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+          window.removeEventListener('resize', handleResize);
+      };
+  }, []);
+
 
   return (
-    <>
+    <windowSizeContext.Provider value={windowWidth}>
      <Header />
      <Info />
      <Features />
      <Testimonials />
      <GetProduct />
      <Footer />
-    </>
+    </windowSizeContext.Provider>
   )
 }
 
